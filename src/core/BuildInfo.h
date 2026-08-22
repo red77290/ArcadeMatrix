f2549bdd6d1ef11ebf327fc0594db253656f6ac5
Red1L <red77290@gmail.com>
2026-08-22 19:45:35 +0200

    chore: update build info stamp

--- src/core/BuildInfo.h
+++ src/core/BuildInfo.h
@@ -3,8 +3,8 @@
 #define FIRMWARE_VERSION "2.0.0"
 #endif
 #ifndef BUILD_GIT_COMMIT
-#define BUILD_GIT_COMMIT "2fbac82"
+#define BUILD_GIT_COMMIT "51dcae4"
 #endif
 #ifndef BUILD_TIMESTAMP
-#define BUILD_TIMESTAMP "2026-08-22 17:44:24 UTC"
+#define BUILD_TIMESTAMP "2026-08-22 17:45:31 UTC"
 #endif
