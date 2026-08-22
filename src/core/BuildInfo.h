031f5723c89ee6509f225b1b5ab51481ef07cb91
Red1L <red77290@gmail.com>
2026-08-22 14:06:12 +0200

    chore: update build info stamp

--- src/core/BuildInfo.h
+++ src/core/BuildInfo.h
@@ -1,7 +1,7 @@
 #pragma once
 #ifndef BUILD_GIT_COMMIT
-#define BUILD_GIT_COMMIT "a63490c"
+#define BUILD_GIT_COMMIT "2437a54"
 #endif
 #ifndef BUILD_TIMESTAMP
-#define BUILD_TIMESTAMP "2026-08-22 12:00:15 UTC"
+#define BUILD_TIMESTAMP "2026-08-22 12:05:36 UTC"
 #endif
