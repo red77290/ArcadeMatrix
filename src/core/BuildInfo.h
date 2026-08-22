e84a8bf4ace1872b21deb21bc191dce5ce857a2f
Red1L <red77290@gmail.com>
2026-08-22 20:23:05 +0200

    fix(webui): improve form grids and rotation settings responsiveness on mobile viewports

--- src/core/BuildInfo.h
+++ src/core/BuildInfo.h
@@ -3,8 +3,8 @@
 #define FIRMWARE_VERSION "2.0.0"
 #endif
 #ifndef BUILD_GIT_COMMIT
-#define BUILD_GIT_COMMIT "f9b7cc6"
+#define BUILD_GIT_COMMIT "8ad914f"
 #endif
 #ifndef BUILD_TIMESTAMP
-#define BUILD_TIMESTAMP "2026-08-22 18:16:46 UTC"
+#define BUILD_TIMESTAMP "2026-08-22 18:22:40 UTC"
 #endif
