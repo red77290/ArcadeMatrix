552135f8b093aefeea6a2fe044d9af8a977643ee
Red1L <red77290@gmail.com>
2026-08-23 14:08:20 +0200

    feat(webui): complete dynamic engine and rotation manager parity on ESP32

--- src/core/BuildInfo.h
+++ src/core/BuildInfo.h
@@ -3,8 +3,8 @@
 #define FIRMWARE_VERSION "2.0.0"
 #endif
 #ifndef BUILD_GIT_COMMIT
-#define BUILD_GIT_COMMIT "c50fb76"
+#define BUILD_GIT_COMMIT "8337fd7"
 #endif
 #ifndef BUILD_TIMESTAMP
-#define BUILD_TIMESTAMP "2026-08-23 12:03:38 UTC"
+#define BUILD_TIMESTAMP "2026-08-23 12:08:00 UTC"
 #endif
