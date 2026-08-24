149ecb079aea7b03e88468836bee874c670e80f8
Red1L <red77290@gmail.com>
2026-08-24 15:29:53 +0200

    docs(webui): add detailed explanations for hardware and system settings tooltips
    - Clarify purpose, optimal values, and hardware impact for all matrix driver options (Latch Blanking, Row Addr Mode, Color Depth, Clk Phase, Single Buffer, etc.)
    - Provide complete trilingual translations (EN, FR, ES)

--- src/core/BuildInfo.h
+++ src/core/BuildInfo.h
@@ -3,8 +3,8 @@
 #define FIRMWARE_VERSION "3.0.0"
 #endif
 #ifndef BUILD_GIT_COMMIT
-#define BUILD_GIT_COMMIT "f8ad5d9"
+#define BUILD_GIT_COMMIT "f0b0691"
 #endif
 #ifndef BUILD_TIMESTAMP
-#define BUILD_TIMESTAMP "2026-08-24 13:24:14 UTC"
+#define BUILD_TIMESTAMP "2026-08-24 13:29:45 UTC"
 #endif
