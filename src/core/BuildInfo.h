f92d30241d37c2cbea3a5b1e719be80a3d5c9b24
Red1L <red77290@gmail.com>
2026-08-25 14:54:45 +0200

    chore: sync BuildInfo.h with latest commit

--- src/core/BuildInfo.h
+++ src/core/BuildInfo.h
@@ -3,8 +3,8 @@
 #define FIRMWARE_VERSION "3.0.0"
 #endif
 #ifndef BUILD_GIT_COMMIT
-#define BUILD_GIT_COMMIT "675b61e"
+#define BUILD_GIT_COMMIT "1ff71d7"
 #endif
 #ifndef BUILD_TIMESTAMP
-#define BUILD_TIMESTAMP "2026-08-25 12:43:45 UTC"
+#define BUILD_TIMESTAMP "2026-08-25 12:54:24 UTC"
 #endif
