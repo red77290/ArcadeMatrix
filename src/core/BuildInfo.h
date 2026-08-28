d012416316d49181c3d4267a1ea5e1d9f453a694
Red1L <red77290@gmail.com>
2026-08-28 17:52:47 +0200

    chore: update BuildInfo.h build metadata

--- src/core/BuildInfo.h
+++ src/core/BuildInfo.h
@@ -3,8 +3,8 @@
 #define FIRMWARE_VERSION "3.0.0"
 #endif
 #ifndef BUILD_GIT_COMMIT
-#define BUILD_GIT_COMMIT "af06be0"
+#define BUILD_GIT_COMMIT "0784a04"
 #endif
 #ifndef BUILD_TIMESTAMP
-#define BUILD_TIMESTAMP "2026-08-28 14:07:57 UTC"
+#define BUILD_TIMESTAMP "2026-08-28 14:33:21 UTC"
 #endif
