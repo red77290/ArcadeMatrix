30a74262899d6cec16da77a961cf7d1858c60ad6
Red1L <red77290@gmail.com>
2026-09-02 09:58:38 +0200

    chore: sync build info

--- src/core/BuildInfo.h
+++ src/core/BuildInfo.h
@@ -3,8 +3,8 @@
 #define FIRMWARE_VERSION "3.1.0-dev"
 #endif
 #ifndef BUILD_GIT_COMMIT
-#define BUILD_GIT_COMMIT "51bba5c"
+#define BUILD_GIT_COMMIT "bab1ce5"
 #endif
 #ifndef BUILD_TIMESTAMP
-#define BUILD_TIMESTAMP "2026-09-02 07:50:55 UTC"
+#define BUILD_TIMESTAMP "2026-09-02 07:58:09 UTC"
 #endif
