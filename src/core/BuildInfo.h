3555f4aea8c9f31ddf5f62e77f566dcc00af6070
Red1L <red77290@gmail.com>
2026-09-02 09:48:30 +0200

    chore(build): update BuildInfo timestamp

--- src/core/BuildInfo.h
+++ src/core/BuildInfo.h
@@ -3,8 +3,8 @@
 #define FIRMWARE_VERSION "3.1.0-dev"
 #endif
 #ifndef BUILD_GIT_COMMIT
-#define BUILD_GIT_COMMIT "88d5138"
+#define BUILD_GIT_COMMIT "51bba5c"
 #endif
 #ifndef BUILD_TIMESTAMP
-#define BUILD_TIMESTAMP "2026-09-01 16:03:37 UTC"
+#define BUILD_TIMESTAMP "2026-09-02 07:46:25 UTC"
 #endif
