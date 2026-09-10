885ee40699b476bed2c04ccd60cd77ce9d71dd57
Red1L <red77290@gmail.com>
2026-09-10 20:54:27 +0200

    fix(ui): sync bundled WebUI with dynamic_engines syntax fix

--- src/core/BuildInfo.h
+++ src/core/BuildInfo.h
@@ -3,8 +3,8 @@
 #define FIRMWARE_VERSION "3.2.0-dev"
 #endif
 #ifndef BUILD_GIT_COMMIT
-#define BUILD_GIT_COMMIT "26e52f8"
+#define BUILD_GIT_COMMIT "8e70d0f"
 #endif
 #ifndef BUILD_TIMESTAMP
-#define BUILD_TIMESTAMP "2026-09-10 12:59:45 UTC"
+#define BUILD_TIMESTAMP "2026-09-10 18:53:56 UTC"
 #endif
