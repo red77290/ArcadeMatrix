d995b0ab31a7bef17aef07923e659104555e4ceb
Red1L <red77290@gmail.com>
2026-09-25 18:31:13 +0200

    feat(webui): add render pipeline selector and i18n translations

--- src/core/BuildInfo.h
+++ src/core/BuildInfo.h
@@ -3,8 +3,8 @@
 #define FIRMWARE_VERSION "3.4.0-dev"
 #endif
 #ifndef BUILD_GIT_COMMIT
-#define BUILD_GIT_COMMIT "a87fd2f"
+#define BUILD_GIT_COMMIT "686ebb8"
 #endif
 #ifndef BUILD_TIMESTAMP
-#define BUILD_TIMESTAMP "2026-09-25 14:22:53 UTC"
+#define BUILD_TIMESTAMP "2026-09-25 16:29:56 UTC"
 #endif
